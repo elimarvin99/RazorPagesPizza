8d0fa79f04d44f8dfb2ecfececab949934a57851
elimarvin99 <elimarvin99@gmail.com>
2021-12-09 11:11:51 -0500

    completed project in building a asp.net website with focus on razor pages, to create a pizza list directory that we can perform CRUD actions on

---
+++ RazorPagesPizza/Pages/Pizza.cshtml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesPizza.Models;
+using RazorPagesPizza.Services;
+
+namespace RazorPagesPizza.Pages
+{
+    public class PizzaModel : PageModel
+    {
+        [BindProperty]
+        public Pizza NewPizza { get; set; } = new Pizza();
+        public List<Pizza> pizzas = new List<Pizza>();
+        public void OnGet()
+        {
+            pizzas = PizzaService.GetAll();
+        }
+        public string GlutenFreeText(Pizza pizza)
+        {
+            if (pizza.IsGlutenFree)
+                return "Gluten Free";
+            return "Not Gluten Free";
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            PizzaService.Add(NewPizza);
+            return RedirectToAction("Get");
+        }
+        public IActionResult OnPostDelete(int id)
+        {
+            PizzaService.Delete(id);
+            return RedirectToAction("Get");
+        }
+    }
+}
